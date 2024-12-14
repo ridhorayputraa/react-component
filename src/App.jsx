aa441971f26dbdfffbce5075b0599bdb14c73daf
Ridho Ray Putra <ridhorayputra033@Gmail.com>
2024-12-14 12:23:35 +0700

    Finishied Todo3

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
 import ToDoForm from "./components/ToDoForm";
 import ToDoList from "./components/ToDoList";
 import ToDoIncrement from "./components/ToDoIncrement";
 
 function App() {
   return (
-    <>
-       <header>
+    <div className="App">
+      <header>
         <h1>Aplikasi ToDo List</h1>
       </header>
 
@@ -17,15 +14,15 @@ function App() {
         <ToDoIncrement />
       </section>
 
-      <section style={{ margin: '1em 0em' }}>
+      <section style={{ margin: "1em 0em" }}>
         <ToDoForm />
         <ToDoList />
       </section>
 
       <footer>
-        <h3>Dibuat oleh orang tidak jelas &copy; 2024</h3>
+        <h3>Dibuat oleh orang tidak jelas &copy; 2022</h3>
       </footer>
-    </>
+    </div>
   );
 }
 
--- src/components/ToDoIncrement.jsx
+++ src/components/ToDoIncrement.jsx
@@ -1,19 +1,26 @@
-import React from "react";
-
-let increment = 0;
+// di sini jadinya harus import useState
+import React, { useState } from 'react';
 
 function ToDoIncrement() {
-  // Kita deklarasi fungsi ketika tombol ditekan
+  // sekarang kita menggunakan useState untuk deklarasi increment
+  // pada saat deklarasi, kita menggunakan array
+  // indeks 0 adalah nama variabel yang ingin dideklarasikan
+  // indeks 1 adalah nama fungsi yang digunakan untuk
+  //   memutasi (mengganti) variabel
+  const [increment, setIncrement] = useState(0);
+
   function buttonNambahClickHandler() {
     console.log(increment);
-    increment = increment + 1;
+    // di sini karena kita ketahui bahwa setIncrement adalah fungsi
+    // kita bisa "invoke" dan memasukkan nilai baru yang diinginkan
+    // anggap seperti "setter"
+    setIncrement(increment + 1);
   }
 
   return (
     <>
-      <span style={{ marginRight: "0.25em" }}>{increment}</span>
+      <span style={{ marginRight: '0.25em' }}>{increment}</span>
       <span>
-        {/* Di sini kita deklarasi onClick untuk menambahkan event ketika tombol diklik */}
         <button onClick={buttonNambahClickHandler}>Nambah</button>
       </span>
     </>
